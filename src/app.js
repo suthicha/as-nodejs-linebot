97c6603947fb43800ad2d138297a36c7973025a6
evosaadlab <evosaadlab@gmail.com>
2020-02-05 20:27:26 +0700

    Add message to database

--- src/app.js
+++ src/app.js
@@ -5,16 +5,125 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const line = require("@line/bot-sdk");
+const fs = require("fs");
+const { LineClient } = require("messaging-api-line");
+const config = require("config");
+const mssql = require("mssql");
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ limit: "1mb", extended: false }));
 app.use(bodyParser.json({ limit: "1mb" }));
 
-app.use("/webhook", (req, res) => {
-  // let reply_token = req.body.events[0].replyToken;
-  console.log(req.body);
-  res.status(200).json({});
+// get accessToken and channelSecret from LINE developers website
+const client = LineClient.connect(config.ACCESS_TOKEN, config.CHANNEL_SECRET);
+
+app.use("/webhook", async (req, res) => {
+  const { messages } = req.body;
+
+  if (messages.hasOwnProperty("message")) {
+    if (messages.message["type"] === "image") {
+      try {
+        const filePath = await downloadImage(messages.message["id"]);
+        await addMessageImageObject(req.body, filePath);
+      } catch {}
+    } else if (messages.message["type"] === "text") {
+      try {
+        await addMessageTextObject(req.body);
+      } catch {}
+    }
+  }
+  res.status(200).json({ messages });
 });
+
+const downloadImage = async id => {
+  return new Promise(async (resolve, reject) => {
+    const messageId = id + ".png";
+    const fileName = "/upload/" + messageId;
+    await client.retrieveMessageContent(id).then(buffer => {
+      fs.writeFile(process.cwd() + fileName, buffer, err => {
+        if (err) throw err;
+        resolve(fileName);
+      });
+    });
+  });
+};
+
+const addMessageImageObject = async (data, url) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { replyToken, messages } = data;
+
+      let pool = new mssql.ConnectionPool(config.CONNECTION);
+      await pool
+        .connect()
+        .then(() => {
+          let req = new mssql.Request(pool);
+          req
+            .input("ReplyToken", replyToken)
+            .input("MessageType", messages["type"])
+            .input("MessageReplyToken", messages["replyToken"])
+            .input("MessageSourceType", messages["source"].type)
+            .input("MessageSourceUserId", messages["source"].userId)
+            .input("MessageMode", messages["mode"])
+            .input("MessageContentType", messages["message"].type)
+            .input("MessageContentId", messages["message"].id)
+            .input("MessageContentText", "")
+            .input(
+              "MessageContentProviderType",
+              messages["message"].contentProvider["type"]
+            )
+            .input("ImageOriginalUrl", url)
+            .execute("SP_AddMessageObject")
+            .then(result => {
+              resolve(result);
+            })
+            .catch(error => reject(error));
+        })
+        .catch(connErr => reject(connErr));
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+const addMessageTextObject = async data => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { replyToken, messages } = data;
+
+      let pool = new mssql.ConnectionPool(config.CONNECTION);
+      await pool
+        .connect()
+        .then(() => {
+          let req = new mssql.Request(pool);
+          req
+            .input("ReplyToken", replyToken)
+            .input("MessageType", messages["type"])
+            .input("MessageReplyToken", messages["replyToken"])
+            .input("MessageSourceType", messages["source"].type)
+            .input("MessageSourceUserId", messages["source"].userId)
+            .input("MessageMode", messages["mode"])
+            .input("MessageContentType", messages["message"].type)
+            .input("MessageContentId", messages["message"].id)
+            .input("MessageContentText", messages["message"].text)
+            .input(
+              "MessageContentProviderType",
+              messages["message"].contentProvider["type"]
+            )
+            .input("ImageOriginalUrl", "")
+            .execute("SP_AddMessageObject")
+            .then(result => {
+              resolve(result);
+            })
+            .catch(error => reject(error));
+        })
+        .catch(connErr => reject(connErr));
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 module.exports = app;
